e8241fe586affeb6d6c3dbdb9caed29b51e01e40
Nidhi Singh <nids0810@gmail.com>
2019-02-05 21:54:35 +0530

    Update algorithm.js

--- algorithm.js
+++ algorithm.js
@@ -228,3 +228,34 @@ function chunking(array, size) {
     return chunks;
 }
 console.log(chunking([1,2,3,4,5], 2));
+
+
+//===============//
+/* 10. Reverse Array */
+//===============//
+function reverseArray(arr) {
+	let newArr = [];
+	for(let i = (arr.length-1); i >= 0; i--) {
+  	newArr.push(arr[i]);
+  }
+  return newArr;
+}
+console.log(reverseArray([1,3,7,2,6,9]));
+
+//using JS method
+function reverseArray(arr) {
+	return arr.reverse();
+}
+console.log(reverseArray([1,3,7,2,6,9]));
+
+//using for loop -best method
+function reverseArray(arr) {
+	for(let i = 0; i < arr.length / 2; i++) {
+  	let temp = arr[i];
+    	arr[i] = arr[arr.length - 1 - i];
+    	arr[arr.length - 1 - i] = temp;
+  }
+  return arr;
+}
+console.log(reverseArray([1,3,7,2,6,9]));
+
