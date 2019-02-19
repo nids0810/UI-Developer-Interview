9ba62d275eb4f3c6aa6df1efe15796553f56d2d3
Nidhi Singh <nids0810@gmail.com>
2019-02-19 16:34:27 +0530

    Update dataStructure.js

--- dataStructure.js
+++ dataStructure.js
@@ -116,3 +116,41 @@ chain.insertHead(4);
 chain.insertHead(5);
         
 console.log(midpoint(chain).data);
+
+// Without keeping node references, check if a linked list is circular
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertHead(data) {
+        this.head = new Node(data, this.head);
+    }
+}
+
+function circular(list) {
+    let moveByOne = list.head;
+    let moveByTwo = list.head;
+
+    while (moveByTwo.next && moveByTwo.next.next) {
+        moveByOne = moveByOne.next;
+        moveByTwo = moveByTwo.next.next;
+        if (moveByTwo === moveByOne) return true;
+    }
+
+    return false;
+}
+
+const chain = new LinkedList();
+chain.insertHead(1);
+chain.insertHead(2);
+chain.insertHead(3);
+chain.head.next.next.next = chain.head;
+        
+console.log(circular(chain));
