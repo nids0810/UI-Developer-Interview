653030100f09d76e1bf49f1f8895d49d47da6dd6
Nidhi Singh <nids0810@gmail.com>
2019-02-19 16:28:38 +0530

    Update dataStructure.js

--- dataStructure.js
+++ dataStructure.js
@@ -40,3 +40,79 @@ const two = new Queue();
     two.enqueue("three");
 
 console.log(weave(one, two));
+
+// Without keeping a counter, return the middle value of a linked list
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertHead(data) {
+        this.head = new Node(data, this.head);
+    }
+}
+
+function midpoint(list) {
+    let moveByOne = list.head;
+    let moveByTwo = list.head;
+
+    while (moveByTwo.next && moveByTwo.next.next) {
+        moveByOne = moveByOne.next;
+        moveByTwo = moveByTwo.next.next;
+    }
+
+    return moveByOne;
+}
+
+const chain = new LinkedList();
+chain.insertHead(1);
+chain.insertHead(2);
+chain.insertHead(3);
+chain.insertHead(4);
+chain.insertHead(5);
+        
+console.log(midpoint(chain).data);
+// Without keeping a counter, return the middle value of a linked list
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertHead(data) {
+        this.head = new Node(data, this.head);
+    }
+}
+
+function midpoint(list) {
+    let moveByOne = list.head;
+    let moveByTwo = list.head;
+
+    while (moveByTwo.next && moveByTwo.next.next) {
+        moveByOne = moveByOne.next;
+        moveByTwo = moveByTwo.next.next;
+    }
+
+    return moveByOne;
+}
+
+const chain = new LinkedList();
+chain.insertHead(1);
+chain.insertHead(2);
+chain.insertHead(3);
+chain.insertHead(4);
+chain.insertHead(5);
+        
+console.log(midpoint(chain).data);
