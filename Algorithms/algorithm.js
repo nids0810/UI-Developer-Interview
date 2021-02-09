d7fc9fa7205960f786e6e57b8b6a8bbb4c0973bf
Nidhi Singh <nids0810@gmail.com>
2021-02-09 20:32:27 +0530

    Update algorithm.js

--- Algorithms/algorithm.js
+++ Algorithms/algorithm.js
@@ -653,3 +653,25 @@ var twoSum = function(arr, target) {
     return newArr;
 }
 console.log(twoSum([6, 3, 2, 5, 4], 9));
+
+// Currying function:
+function sum(a,b,c) {
+	return a+b+c;
+}
+var currying = curry(sum);
+
+function curry(func) {
+	return function curried(...args) {
+  	if(args.length >= func.length) {
+    	return func.apply(this, args);
+    } else {
+    	return function(...args2) {
+      	return curried.apply(this, args.concat(args2));
+      }
+    }
+  }
+}
+
+console.log(currying(1,2,3)); //6
+console.log(currying(1)(2,3)); //6
+console.log(currying(1)(2)(3)); //6
